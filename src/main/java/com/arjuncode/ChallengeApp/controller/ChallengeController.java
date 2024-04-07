91ff624221c88478a605258952cdb300070081d4
Mallikarjunaiah B M <vpmallikarjuna03@gmail.com>
2024-04-07 20:49:22 +0530

    Get and Post Request for basic challenge

--- src/main/java/com/arjuncode/ChallengeApp/controller/ChallengeController.java
+++ src/main/java/com/arjuncode/ChallengeApp/controller/ChallengeController.java
@@ -2,6 +2,8 @@ package com.arjuncode.ChallengeApp.controller;
 
 import com.arjuncode.ChallengeApp.Challenge;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -22,5 +24,11 @@ public class ChallengeController {
     @GetMapping("/challenges")
     public List<Challenge> getAllChallenges(){
         return challenges;
+    }
+
+    @PostMapping("/challenges")
+    public String addChallenge(@RequestBody Challenge challenge){
+        challenges.add(challenge);
+        return "Challenge added successfully";
     }
 }

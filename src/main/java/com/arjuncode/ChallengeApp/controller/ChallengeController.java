e870ff148c0513c6c30889c2bf2f04f711b81115
Mallikarjunaiah B M <vpmallikarjuna03@gmail.com>
2024-04-07 20:34:58 +0530

    Initial Challenge Application configs and setup

---
+++ src/main/java/com/arjuncode/ChallengeApp/controller/ChallengeController.java
@@ -0,0 +1,26 @@
+package com.arjuncode.ChallengeApp.controller;
+
+import com.arjuncode.ChallengeApp.Challenge;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ChallengeController {
+
+    //we need to store challenges
+    private List<Challenge> challenges = new ArrayList<>();
+
+    public ChallengeController() {
+        //create new object of challenge class
+        Challenge challenge1 = new Challenge(1L, "January", "This is the January month challenge");
+        challenges.add(challenge1);
+    }
+
+    @GetMapping("/challenges")
+    public List<Challenge> getAllChallenges(){
+        return challenges;
+    }
+}
